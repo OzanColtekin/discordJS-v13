ef4cdaaa7e86687971c43b1865737b22431c4905
OzanColtekin <ozan.coltekin@outlook.com>
2022-04-17 01:31:34 +0300

    First event added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import { Client,Collection } from "discord.js";
+import { Client, Collection } from "discord.js";
 import "dotenv/config";
 import { readdirSync } from "fs";
 
 const client = new Client({
     intents:["GUILDS","GUILD_MEMBERS","GUILD_MESSAGES"]
+})
+
+readdirSync("./events").forEach(async file =>{
+    const event = await import(`./events/${file}`).then(m=> m.default)
+    event(client)
 })
 
 client.login(process.env.token)
