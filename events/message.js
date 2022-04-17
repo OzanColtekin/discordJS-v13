d9c7da53d4404efea4fc75f8e69b67ccb72a0df6
OzanColtekin <ozan.coltekin@outlook.com>
2022-04-18 00:48:14 +0300

    Commands enabled

--- events/message.js
+++ events/message.js
@@ -1,5 +1,10 @@
 export default client =>{
-    client.on("message",message =>{
-        console.log("mesaj geldi.")
+    const prefix = process.env.prefix
+    client.on("messageCreate",message =>{
+        if(message.content.startsWith(prefix) == false) return 0;
+        const args = message.content.slice(prefix.length).trim().split(/ +/)
+        const commandName = args.shift().toLowerCase()
+        console.log(commandName)
+
     })
 }
